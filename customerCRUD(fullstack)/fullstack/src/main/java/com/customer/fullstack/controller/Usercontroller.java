c99b153a35e266caff4ae0afe24954027c3bd9a6
M.G.Hirunisha Nirmani <127573423+Hirunisha-Nirmani@users.noreply.github.com>
2024-11-03 00:15:00 +0530

    Add files via upload

---
+++ customerCRUD(fullstack)/fullstack/src/main/java/com/customer/fullstack/controller/Usercontroller.java
@@ -0,0 +1,32 @@
+package com.customer.fullstack.controller;
+
+
+import com.customer.fullstack.model.User;
+import com.customer.fullstack.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class Usercontroller {
+
+    @Autowired
+    private UserRepository userrepo;
+
+    @PostMapping("/add_user")
+    User newUser(@RequestBody User newUser)
+    {
+        return  userrepo.save(newUser);
+    }
+    @GetMapping("/all_user")
+    List<User> getAllUser()
+    {
+        return userrepo.findAll();
+    }
+
+}
+
---
+++ customerCRUD(fullstack)/fullstack/src/main/java/com/customer/fullstack/model/User.java
@@ -0,0 +1,48 @@
+package com.customer.fullstack.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private long userId;
+    private  String userName;
+    private  String name;
+    private  String email;
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
